f69dfe8c317e422e19c1ddb4cc7574324dec2c7e
akash singh <aakashprataps832@gmail.com>
2024-06-03 15:36:42 +0530

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.example.views.Welcome;
+
+public class Main {
+    public static void main(String[] args) {
+        Welcome welcome = new Welcome();
+
+        do {
+
+            welcome.welcomeScreen();
+
+        }while (true);
+
+    }
+    }
